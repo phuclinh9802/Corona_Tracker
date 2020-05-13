4c0de89d22ef5212f9e4384529d1278bf8fe3824
Phillip <anicedream2002@gmail.com>
2020-05-13 01:01:09 -0500

    Modified ChartComponent - Unfinished

--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { CoronaService} from '../corona.service'
+import { CoronaService} from '../corona.service';
+import { Chart } from 'node_modules/chart.js'
 
 @Component({
   selector: 'app-chart',
@@ -14,12 +15,17 @@ export class ChartComponent implements OnInit {
   country: any;
 
   confirmArray: Number[] = [];
+  recoverArray: Number[] = [];
+  deathArray: Number[] = [];
   constructor(private corona : CoronaService) { }
 
   ngOnInit(): void {
     this.corona.getCountries().subscribe((data) => {
       this.countries = data;
     })
+
+    
+
   }
 
   getCountry(country: any) {
@@ -29,9 +35,48 @@ export class ChartComponent implements OnInit {
     this.corona.getInfoByCountries(this.country).subscribe((data) => {
       for (let element of data) {
         this.confirmArray.push(element.Confirmed);
+        this.recoverArray.push(element.Recovered);
+        this.deathArray.push(element.Deaths);
       }
     })
+
+    console.log(this.deathArray);
     // alert(this.country);
+    // var myChart = new Chart("myChart", {
+    //   type: 'line',
+    //   data: {
+    //     labels: this.country,
+    //     datasets: [
+    //       { 
+    //         data: this.confirmArray,
+    //         label: "Confirmed",
+    //         borderColor: "green",
+    //         fill: false
+            
+    //       },
+    //       { 
+    //         data: this.recoverArray,
+    //         label: "Recovered",
+    //         borderColor: "yellow",
+    //         fill: false
+            
+    //       },
+    //       { 
+    //         data: this.deathArray,
+    //         label: "Deaths",
+    //         borderColor: "red",
+    //         fill: false
+    //       }
+    //     ]
+    //   },
+    //   // options: {
+    //   //   responsive: true,
+    //   //   animation: {
+    //   //     duration: 0.5
+    //   //   }
+    //   // }
+    // });
+    //this.clear();
   }
 
   clear(): void {
