a3e5a5ebfc3c320a862f839b4bfc9578580e7a33
Phillip <anicedream2002@gmail.com>
2020-05-13 13:27:14 -0500

    Added and Modified Chart.hjs + fixed bugs: not displaying Chart until resize and chart hovering bug

--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CoronaService} from '../corona.service';
 import { Chart } from 'node_modules/chart.js'
+import { discardPeriodicTasks } from '@angular/core/testing';
 
 @Component({
   selector: 'app-chart',
@@ -17,15 +18,14 @@ export class ChartComponent implements OnInit {
   confirmArray: Number[] = [];
   recoverArray: Number[] = [];
   deathArray: Number[] = [];
+  days: Number[] = [];
+  day: any = 0;
   constructor(private corona : CoronaService) { }
 
   ngOnInit(): void {
     this.corona.getCountries().subscribe((data) => {
       this.countries = data;
     })
-
-    
-
   }
 
   getCountry(country: any) {
@@ -34,53 +34,91 @@ export class ChartComponent implements OnInit {
   getInfo(): void {
     this.corona.getInfoByCountries(this.country).subscribe((data) => {
       for (let element of data) {
+        this.day = this.day + 1;
+        this.days.push(this.day);
         this.confirmArray.push(element.Confirmed);
         this.recoverArray.push(element.Recovered);
         this.deathArray.push(element.Deaths);
       }
     })
+  }
+  showChart(): void {
+    var button = document.getElementById("showButton");
+    button.addEventListener("click", function(){
+      myChart.destroy();
+    });
+    var myChart = new Chart("myChart", {
+      type: 'line',
+      data: {
 
-    console.log(this.deathArray);
-    // alert(this.country);
-    // var myChart = new Chart("myChart", {
-    //   type: 'line',
-    //   data: {
-    //     labels: this.country,
-    //     datasets: [
-    //       { 
-    //         data: this.confirmArray,
-    //         label: "Confirmed",
-    //         borderColor: "green",
-    //         fill: false
+        labels: this.days,
+        datasets: [
+          { 
+            data: this.confirmArray,
+            label: "Confirmed",
+            borderColor: "green",
+            fontColor: "#eee",
+            fill: false
             
-    //       },
-    //       { 
-    //         data: this.recoverArray,
-    //         label: "Recovered",
-    //         borderColor: "yellow",
-    //         fill: false
+          },
+          { 
+            data: this.recoverArray,
+            label: "Recovered",
+            borderColor: "yellow",
+            fontColor: "#eee",
+            fill: false
             
-    //       },
-    //       { 
-    //         data: this.deathArray,
-    //         label: "Deaths",
-    //         borderColor: "red",
-    //         fill: false
-    //       }
-    //     ]
-    //   },
-    //   // options: {
-    //   //   responsive: true,
-    //   //   animation: {
-    //   //     duration: 0.5
-    //   //   }
-    //   // }
-    // });
+          },
+          { 
+            data: this.deathArray,
+            label: "Deaths",
+            borderColor: "red",
+            fontColor: "#eee",
+            fill: false
+          }
+        ]
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+          xAxes: [{
+            gridLines: {
+              display: true,
+              color: '#aaa',
+              lineWidth: 1
+            },
+            ticks: {
+              fontColor: "white",
+            }
+          }],
+          yAxes: [{
+            gridLines: {
+              display: true,
+              color: '#aaa',
+              lineWidth: 1
+            },
+            ticks: {
+              fontColor: "white",
+            }
+          }]
+        }
+      }
+      // options: {
+      //   responsive: true,
+      //   animation: {
+      //     duration: 0.5
+      //   }
+      // }
+    });
     //this.clear();
   }
 
   clear(): void {
     this.confirmArray = [];
+    this.deathArray = [];
+    this.recoverArray = [];
+    this.days = [];
   }
 
 }
